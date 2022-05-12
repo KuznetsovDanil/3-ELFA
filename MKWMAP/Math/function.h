a3765009e248ac575e021017ec55d364bc3bab27
trutre0 <trutre@bk.ru>
2022-05-12 22:09:24 +0700

    start

--- MKWMAP/Math/function.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#define _USE_MATH_DEFINES
-#include <math.h> // по хорошему надо использовать cmath но с ним _USE_MATH_DEFINES не робит
-
-
-using namespace std;
-double test_function(double, int);
-double answer(double, double, int);
